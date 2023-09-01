c21fa34331c5bc85e8f45e59d23699fb33d8faa4
Optimalsten <soa@prg.spb.ru>
2023-09-01 23:58:30 +0300

    new data Ecample007 Vagic

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 50, ya = 1,
+int xa = 40, ya = 1,
     xb = 1, yb = 30,
-    xc = 100, yc = 30;
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
