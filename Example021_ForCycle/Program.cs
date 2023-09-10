6dd2f7b6a30f43c9ad4776237cc32b58845fc7db
Optimalsten <soa@prg.spb.ru>
2023-09-11 00:20:40 +0300

    feat: add to project programm text Example 021 ForCycle

---
+++ Example021_ForCycle/Program.cs
@@ -0,0 +1,125 @@
+﻿// Вид 4 - метод 4-го типа, "что-то принимают на вход, что-то возвращает"
+// ЗНАЧИТ тип данных, который ожидаем назад, и тип параметров
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими"К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012...
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// Алгоритм сортировки методом выбора (методом МиниМакса или выбора макс-ного/миним-ного)
+// 6 8 3 2 1 4 5 7
+// 1 8 3 2 6 4 5 7
+// 1 2 3 8 6 4 5 7
+// 1 2 3 8 6 4 5 7
+// 1 2 3 4 6 8 5 7
+// 1 2 3 4 5 8 6 7
+// 1 2 3 4 5 6 8 7
+// 1 2 3 4 5 6 7 8
+
+// 1. Найти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции
+// 3. Повторить пока есть неотсортированные элементы
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectMinSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectMaxSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+int[] arr1 = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+PrintArray(arr1);
+SelectMinSort(arr1);
+PrintArray(arr1);
+
+int[] arr2 = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+PrintArray(arr2);
+SelectMaxSort(arr2);
+PrintArray(arr2);
+
