6a80770f43bfe4dde7a214a7ee0ad0994f6ed1fc
Optimalsten <soa@prg.spb.ru>
2023-09-16 18:35:39 +0300

    feat: rename directory EXAMPLE 033 (Factorial) on LEKTION 5

---
+++ Example033_Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿// Факториал
+// 5! = 5 * 4 * 3 * 2 * 1
+//      5 * 4!
+//          4 * 3!
+//              3 * 2!
+// т.е. функция, которая вызывает сама себя
+
+double Factorial(int n)
+{
+    // 1! = 1 (кстати 0! = 1 тоже)
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");    
+}
