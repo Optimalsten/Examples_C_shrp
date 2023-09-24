652e717e2d30d9e826c4e53e55644742313583af
Optimalsten <soa@prg.spb.ru>
2023-09-24 22:59:36 +0300

    feat: add to project Example051 (TypeVar) on Lesson 5

---
+++ Example051_TypeVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+// int a = 12;
+//TODO: Демонстрация работы с .Name будет писать Int32
+// Console.WriteLine(a.GetType().Name);
+
+var a = 12;
+//TODO: Демонстрация работы без .Name будет писать System.Int32
+// Console.WriteLine(a.GetType());
+
+var data = new int[] { 1, 2, 3, 4 }
+        .Where(e => e > 0)
+        .Select(e => new { q = e, w = e + 1 });
+Console.WriteLine(data.GetType().Name);
+Console.WriteLine(data.GetType());
